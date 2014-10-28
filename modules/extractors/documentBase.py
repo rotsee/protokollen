cbb341025e0d38d8a17793fc64da1031598b6870
Rotsee <leo_wallentin@hotmail.com>
2014-10-28 14:32:46 +0100

    Add Page class and page getter

--- modules/extractors/documentBase.py
+++ modules/extractors/documentBase.py
@@ -2,11 +2,54 @@
 """This module contains the base class for all document classes.
 """
 
-from utils import get_date_from_text
+from modules.utils import get_date_from_text
+import logging
+
+
+class DocumentType(object):
+    """Store document types.
+    """
+
+    UNKNOWN = 0
+    MEETING_MINUTES = 1
+    AGENDA = 2
+    ATTACHMENT = 3
+
+
+class Page(object):
+    """Base class for page classes for various document types.
+       Used in extractor classes.
+    """
+
+    def __init__(self):
+        pass
+
+    def get_text(self):
+        """Should return all plain text in this page.
+        """
+        raise NotImplementedError('must be overridden by child classes')
+
+    def get_date(self):
+        """Return a best guess for the date these minutes were taken,
+           typically by looking in the page header. Do NOT rely on
+           document metadata!
+
+           Returns a datetime object
+        """
+        raise NotImplementedError('must be overridden by child classes')
+
+    def get_type(self):
+        """Return a best guess for the type of document this page belongs to.
+           This should be done on page level, as one file can contain multiple
+           documents (e.g. meeting minutes, attachments and invitation).
+
+           Returns a DocumentType, e.g. DocumentType.MEETING_MINUTES
+        """
+        raise NotImplementedError('must be overridden by child classes')
 
 
 class ExtractorBase(object):
-    """Base class extractor classes for various document types.
+    """Base class for extractor classes for various document types.
        Contains some common functionality, and definitions of
        mandatory methods for sub classes.
     """
@@ -21,23 +64,21 @@ class ExtractorBase(object):
         raise NotImplementedError('must be overridden by child classes')
 
     def get_text(self):
-    	  """Should returns all text content from the document as plain text.
+        """Should return all text content from the document as plain text.
         """
         raise NotImplementedError('must be overridden by child classes')
 
-    def get_header_text(self):
-      """Should return page headers as raw text.
-         This can be used to guess meeting dates, and document type,
-         e.g. fins out if something is an attachment or actual minutes
-      """
-      raise NotImplementedError('must be overridden by child classes')
+    def get_next_page(self):
+        """Should yield a Page object
+        """
+        raise NotImplementedError('must be overridden by child classes')
 
     def get_date(self):
-        """Should return a best guess for the date of the meeting
-           this document refers to. This is normally a found in the
-           header.
+        """Return a best guess for the date of the meeting
+           this document refers to, based on the whole text.
 
-           utils.get_date_from_text can be used as a fallback method.
+           Used a fallback when Page.get_date does not return a
+           page specific date.
 
            Do NOT rely on metadata to get the date, as this is more
            often than not wrong.
