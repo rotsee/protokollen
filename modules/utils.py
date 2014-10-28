13332b1801d86583c7ae060202c6ccc742f4c7b8
Rotsee <leo_wallentin@hotmail.com>
2014-10-28 14:31:35 +0100

    get_single_date_from_text

--- modules/utils.py
+++ modules/utils.py
@@ -4,6 +4,37 @@
 """
 import re
 
+datePatterns = [
+    '\d{4}-\d{1,2}-\d{1,2}',  # 2014-04-24 or 2014-4-24
+    '\d{4}/\d{1,2}/\d{1,2}',  # 2014/04/24 or 2014/4/24
+    '\d{1,2}\.\d{1,2}\.\d{4}',  # 24.4.2014
+]
+"""Date patterns for get_first_date_from_text and get_date_from_text
+   “Myndigheternas skrivregler” recommends using 2005-04-14 or 15.4.2005
+   in document headers.
+"""
+
+
+def _get_dates_list(text):
+    dates = []
+    for pattern in datePatterns:
+        dates.extend(re.compile(pattern).findall(text))
+    return dates
+
+
+def get_single_date_from_text(text):
+    """ Returns the date portion from a short text, that is known to contain
+        one and one date. None if the number of matches is not 1
+    """
+
+    dates = _get_dates_list(text)
+
+    if (len(dates) == 1):
+        import dateutil.parser as dateParser
+        return dateParser.parse(dates[0], fuzzy=True)
+    else:
+        return None
+
 
 def get_date_from_text(text):
     """ Takes a bunch of text, looks for dates and returns the most common one
@@ -12,20 +43,7 @@ def get_date_from_text(text):
         documents.
     """
 
-    # Specify available date formats (regex)
-    # This part should probably be improved with more patterns
-    # “Myndigheternas skrivregler” recommends using 2005-04-14 or 15.4.2005
-    # in document headers
-    datePatterns = [
-        '\d{4}-\d{1,2}-\d{1,2}',  # 2014-04-24 or 2014-4-24
-        '\d{4}/\d{1,2}/\d{1,2}',  # 2014/04/24 or 2014/4/24
-        '\d{1,2}\.\d{1,2}\.\d{4}',  # 24.4.2014
-    ]
-
-    # Parse the text for these patterns and store matches in a list
-    dates = []
-    for pattern in datePatterns:
-        dates = dates + re.compile(pattern).findall(text)
+    dates = _get_dates_list(text)
 
     if (len(dates) > 0):
         from collections import Counter
