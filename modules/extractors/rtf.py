cf32d6c9fcc241a94d96c212f83a2acda88ab999
Rotsee <leo_wallentin@hotmail.com>
2014-10-30 15:08:40 +0100

    Make RTF module work

--- modules/extractors/rtf.py
+++ modules/extractors/rtf.py
@@ -3,23 +3,58 @@
    RTF files
 """
 
-from documentBase import ExtractorBase
+from modules.extractors.doc import DocExtractor
 from modules.metadata import Metadata
+import rtf2xml.ParseRtf
 
 
-class RtfExtractor(ExtractorBase):
-    """Class for getting plain text from a Microsoft Word file.
+class RtfExtractor(DocExtractor):
+    """Class for handling RTF file data extraction.
+
+       We will mostly be using the same logic as for MSDoc files, but we need
+       another way to extract metadata, as wVSummary doesn't work on RTF
     """
 
-    def get_metadata(self):
-        """Returns a .modules.metadata.Metadata object
-        """
-        pass
+    NS_RTF = "http://rtf2xml.sourceforge.net/"
 
-    def get_text(self):
-        """Returns all text content from the document as plain text.
+    def namespace(self, element):
+        import re
+        m = re.match('\{.*\}', element.tag)
+        return m.group(0) if m else ''
+
+    def tag_name(self, element):
+        import re
+        m = re.match('\{.*\}(.*)', element.tag)
+        return m.group(1) if m else ''
+
+    def get_metadata(self):
+        """Returns a metadata.Metadata object
         """
-        pass
+        import os
+        temp_filename = os.path.join("temp", "tmp.rtf.xml")
+
+        parse_obj = rtf2xml.ParseRtf.ParseRtf(in_file=self.path,
+                                              out_file=temp_filename)
+        parse_obj.parse_rtf()
+        metadata = Metadata()
+
+        import xml.etree.ElementTree as ET
+        tree = ET.parse(temp_filename)
+        root = tree.getroot()
+        section = root.find(".//{%s}doc-information" % self.NS_RTF)
+        if len(section) > 0:
+            for tag in section.iterfind(".//*"):
+                tag_name = self.tag_name(tag)
+                if tag.text is not None:
+                    metadata.add({tag_name: tag.text})
+                elif tag.get("year") is not None and tag.get("year") != "0":
+                    date_str = tag.get("year") + "-" + tag.get("month").zfill(2) + "-" + tag.get("day").zfill(2)
+                    metadata.add({tag_name: date_str})
+
+
+        os.unlink(temp_filename)
+        return metadata
+
 
 if __name__ == "__main__":
     print "This module is only intended to be called from other scripts."
