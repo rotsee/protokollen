17c285bf9ea55bdaa5b222bf768ffe943d237036
rotsee <leo_wallentin@hotmail.com>
2014-11-04 15:17:49 +0100

    Add put method

--- modules/databases/elasticsearchdb.py
+++ modules/databases/elasticsearchdb.py
@@ -4,6 +4,7 @@
 
 from modules.databases.database import Database
 from elasticsearch import Elasticsearch
+from elasticsearch.exceptions import NotFoundError
 
 
 class ElasticSearch(Database):
@@ -18,12 +19,28 @@ class ElasticSearch(Database):
                                  "port": port,
                                  }])
 
+    def _get_id(self, id):
+        try:
+            res = self.es.get(index=self.index, doc_type=self.doctype, id=id)
+            return res["_source"]
+        except NotFoundError:
+            return None
+
     def put(self, key, attr, value):
+        body = self._get_id(key) or {}
+        body[attr] = value
+        self.es.index(index=self.index,
+                      doc_type=self.doctype,
+                      id=key,
+                      body=body)
         pass
 
     def get(self, key, attr):
-        res = self.es.get(index=self.index, doc_type=self.doctype, id=key)
-        return res["_source"][attr]
+        res = self._get_id(key)
+        if res is not None and attr in res:
+            return res[attr]
+        else:
+            return None
 
 if __name__ == "__main__":
     print "This module is only intended to be called from other scripts."
