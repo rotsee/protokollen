0310f822fbdfbe0a09bbf19da5d992226353dfd8
rotsee <leo_wallentin@hotmail.com>
2014-11-05 10:40:56 +0100

    Add database settings. No credentials are mandatory

--- login.template.py
+++ login.template.py
@@ -1,17 +1,24 @@
-############### REQUIRED #######################
-#
-#Amazon S3 credentials
-#
-access_key_id = "ACCES KEY ID"
-secret_access_key = "SECRET ACCESS KEY"
-access_token = None
-bucket_name = "BUCKET NAME"
-text_bucket_name = "BUCKET NAME"
+########################################################################
+# Storage credentials, if you want to use a remote storage for files   #
+# and extracted text chunks                                            #
+########################################################################
+#access_key_id = "ACCES KEY ID"
+#secret_access_key = "SECRET ACCESS KEY"
+#access_token = None
+#bucket_name = "BUCKET NAME"  # For Amazon S3
+#text_bucket_name = "BUCKET NAME"  # For Amazon S3
 
-############### OPTIONAL #######################
-#
-#Google API credentials
-#
+########################################################################
+# Database credentials, if you want to index downloaded files          #
+# and extracted texts in a database                                    #
+########################################################################
+#db_server = 'localhost'
+#db_port = 9200
+
+########################################################################
+# Google API credentials, if you want to fetch your harvesting         #
+# instructions from a Google Spreadsheets sheet                        #
+########################################################################
 #google_client_email = "GOOGLE CLIENT EMAIL"
 #google_p12_file = "google_api.p12"
 #google_spreadsheet_key = "SPREADSHEETKEY"
--- settings.py
+++ settings.py
@@ -2,12 +2,13 @@
 """This file contains some commons settings for all scripts
 """
 from modules import storage
+from modules import databases
 from modules.download import FileType
 
-allowedFiletypes = [FileType.PDF, FileType.DOC, FileType.DOCX]
+allowedFiletypes = [FileType.PDF, FileType.DOC, FileType.DOCX, FileType.RTF]
 """What filetypes should we download and store?"""
 
-user_agent = "ProtoKollen; ProtoCollection (http://protokollen.net)"
+user_agent = "Protokollen (http://protokollen.net)"
 """How do we identify ourselves?"""
 
 Storage = storage.S3Storage
@@ -19,4 +20,11 @@ Storage = storage.S3Storage
  To use Dropbox, try
  Storage = storage.DropboxUploader
  (and set the parameters in login.py accordingly)
+"""
+
+Database = databases.elasticsearchdb.ElasticSearch
+"""
+ Remove or set to None to disable database indexing
+
+ You need to set the database parameters in login.py
 """
