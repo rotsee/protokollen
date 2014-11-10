c749facb3b5e3ba8db68d054ac0e11a9bcfe0ff5
rotsee <leo_wallentin@hotmail.com>
2014-11-10 13:51:15 +0100

    Added a dummy db class for debugging

---
+++ modules/databases/debuggerdb.py
@@ -0,0 +1,29 @@
+#coding=utf-8
+"""This file contains a fake DB class, for debugging purposes
+"""
+
+from modules.databases.database import Database
+
+
+class Debugger(Database):
+    """This class acts like a Database object,
+       but dumps stuff to the screen, rather than storing them.
+    """
+
+    def __init__(self, server, table):
+        self.table = table
+        pass
+
+    def put(self, key, attr, value):
+        """Will return True if a value was written, or False
+        """
+        print "Storing %s in %s.%s.%s" % (value, self.table, key, attr)
+        return True
+
+    def get(self, key, attr):
+        return None
+
+if __name__ == "__main__":
+    print "This module is only intended to be called from other scripts."
+    import sys
+    sys.exit()
