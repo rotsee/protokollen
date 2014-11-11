4c4e4b8caee4a2564741192fad4acfa4742daa1c
rotsee <leo_wallentin@hotmail.com>
2014-11-11 10:56:14 +0100

    minor cleanup

--- modules/xmp.py
+++ modules/xmp.py
@@ -1,7 +1,7 @@
 #coding=utf-8
 """Parses XMP metadata from PDF files.
-   By Matt Swain, from:
-   http://blog.matt-swain.com/post/25650072381/a-lightweight-xmp-parser-for-extracting-pdf-metadata-in
+
+   By Matt Swain. Released under the MIT license.
 """
 
 from collections import defaultdict
@@ -10,17 +10,18 @@ from xml.etree import ElementTree as ET
 RDF_NS = '{http://www.w3.org/1999/02/22-rdf-syntax-ns#}'
 XML_NS = '{http://www.w3.org/XML/1998/namespace}'
 NS_MAP = {
-    'http://www.w3.org/1999/02/22-rdf-syntax-ns#'    : 'rdf',
-    'http://purl.org/dc/elements/1.1/'               : 'dc',
-    'http://ns.adobe.com/xap/1.0/'                   : 'xap',
-    'http://ns.adobe.com/pdf/1.3/'                   : 'pdf',
-    'http://ns.adobe.com/xap/1.0/mm/'                : 'xapmm',
-    'http://ns.adobe.com/pdfx/1.3/'                  : 'pdfx',
-    'http://prismstandard.org/namespaces/basic/2.0/' : 'prism',
-    'http://crossref.org/crossmark/1.0/'             : 'crossmark',
-    'http://ns.adobe.com/xap/1.0/rights/'            : 'rights',
-    'http://www.w3.org/XML/1998/namespace'           : 'xml'
+    'http://www.w3.org/1999/02/22-rdf-syntax-ns#': 'rdf',
+    'http://purl.org/dc/elements/1.1/': 'dc',
+    'http://ns.adobe.com/xap/1.0/': 'xap',
+    'http://ns.adobe.com/pdf/1.3/': 'pdf',
+    'http://ns.adobe.com/xap/1.0/mm/': 'xapmm',
+    'http://ns.adobe.com/pdfx/1.3/': 'pdfx',
+    'http://prismstandard.org/namespaces/basic/2.0/': 'prism',
+    'http://crossref.org/crossmark/1.0/': 'crossmark',
+    'http://ns.adobe.com/xap/1.0/rights/': 'rights',
+    'http://www.w3.org/XML/1998/namespace': 'xml'
 }
+
 
 class XmpParser(object):
     """
@@ -34,15 +35,15 @@ class XmpParser(object):
 
     def __init__(self, xmp):
         self.tree = ET.XML(xmp)
-        self.rdftree = self.tree.find(RDF_NS+'RDF')
+        self.rdftree = self.tree.find(RDF_NS + 'RDF')
 
     @property
     def meta(self):
         """ A dictionary of all the parsed metadata. """
         meta = defaultdict(dict)
-        for desc in self.rdftree.findall(RDF_NS+'Description'):
+        for desc in self.rdftree.findall(RDF_NS + 'Description'):
             for el in desc.getchildren():
-                ns, tag =  self._parse_tag(el)
+                ns, tag = self._parse_tag(el)
                 value = self._parse_value(el)
                 meta[ns][tag] = value
         return dict(meta)
@@ -52,29 +53,31 @@ class XmpParser(object):
         ns = None
         tag = el.tag
         if tag[0] == "{":
-            ns, tag = tag[1:].split('}',1)
+            ns, tag = tag[1:].split('}', 1)
             if ns in NS_MAP:
                 ns = NS_MAP[ns]
         return ns, tag
 
     def _parse_value(self, el):
         """ Extract the metadata value from an element. """
-        if el.find(RDF_NS+'Bag') is not None:
+        if el.find(RDF_NS + 'Bag') is not None:
             value = []
-            for li in el.findall(RDF_NS+'Bag/'+RDF_NS+'li'):
+            for li in el.findall(RDF_NS + 'Bag/' + RDF_NS + 'li'):
                 value.append(li.text)
-        elif el.find(RDF_NS+'Seq') is not None:
+        elif el.find(RDF_NS + 'Seq') is not None:
             value = []
-            for li in el.findall(RDF_NS+'Seq/'+RDF_NS+'li'):
+            for li in el.findall(RDF_NS + 'Seq/' + RDF_NS + 'li'):
                 value.append(li.text)
-        elif el.find(RDF_NS+'Alt') is not None:
+        elif el.find(RDF_NS + 'Alt') is not None:
             value = {}
-            for li in el.findall(RDF_NS+'Alt/'+RDF_NS+'li'):
-                value[li.get(XML_NS+'lang')] = li.text
+            for li in el.findall(RDF_NS + 'Alt/' + RDF_NS + 'li'):
+                value[li.get(XML_NS + 'lang')] = li.text
         else:
             value = el.text
         return value
 
+
 def xmp_to_dict(xmp):
-    """ Shorthand function for parsing an XMP string into a python dictionary. """
+    """ Shorthand function for parsing an XMP string into a python dictionary.
+    """
     return XmpParser(xmp).meta
