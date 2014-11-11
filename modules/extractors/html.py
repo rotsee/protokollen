d5c7a97d5c51b7c1476d79482fd25776b03c17f0
rotsee <leo_wallentin@hotmail.com>
2014-11-11 14:56:48 +0100

    HTML extractor skeleton

---
+++ modules/extractors/html.py
@@ -0,0 +1,43 @@
+#coding=utf-8
+"""This module includes tools for handling and extracting text from
+   HTML files
+"""
+
+from modules.extractors.documentBase import ExtractorBase, Page
+from modules.metadata import Metadata
+
+
+class HtmlPage(Page):
+
+    def __init__(self):
+        pass
+
+    def get_text(self):
+        """An HTML page is treater as one, single, long page,
+           at least for now
+        """
+        return self._text
+
+
+class HtmlExtractor(ExtractorBase):
+    """Class for handling HTML file data extraction.
+    """
+
+    def get_next_page(self):
+        page = HtmlPage()
+        page._text = self.get_text()
+        yield page
+
+    def get_text(self):
+        return None
+
+    def get_metadata(self):
+        """Returns a metadata.Metadata object
+        """
+        return None
+
+
+if __name__ == "__main__":
+    print "This module is only intended to be called from other scripts."
+    import sys
+    sys.exit()
