72101f3fb0494c55fcca87400053069f251c97e4
rotsee <leo_wallentin@hotmail.com>
2014-12-13 14:27:23 +0100

    make work

--- maintenance/erase.py
+++ maintenance/erase.py
@@ -3,16 +3,17 @@
 """This script will delete a file and all of its documents,
    from both the storage, and the database.
 
-   You can give the filename (key) directly, or use a file
-   containg one key on each line, or use a Google Spreadsheet.
-   The spreadsheet should contain all filename in one column,
-   and the column must contain a header (any header will do).
+   For each file, you need to provide its path fragments, if any, and name,
+   e.g. `"Enköpings kommun", 3fce5e1f6db1bd90120a5e48e468a68b.pdf`
+
+   You can use the command line, a file or a Google Spreadsheet document
+   to provide the file(s) to be deleted.
 """
 
 import settings
 
 from modules.interface import Interface
-from modules.datasheet import CSVFile, GoogleSheet, DataSet
+from modules.datasheet import CSVFile, GoogleSheet, DataSet, HeaderlessDataSet
 from modules.databases.debuggerdb import DebuggerDB
 
 
@@ -23,19 +24,21 @@ def main():
         "short": "-f", "long": "--file",
         "dest": "filename",
         "type": str,
-        "help": """File containing a new-line separated list of
-                   keys to be deleted."""
+        "help": """A headerless CSV file containing paths and names
+                   of the files to delete."""
     }, {
-        "short": "-k", "long": "--key",
-        "dest": "key",
+        "short": "-p", "long": "--path",
+        "dest": "path_fragments",
+        "nargs": "+",
         "type": str,
-        "help": "A key to be deleted."
+        "help": """Path fragments and file name,
+                   e.g. \"Enköpings kommun\" abcdefgh.pdf"""
     }, {
         "short": "-g", "long": "--google-sheet",
         "dest": "google_spreadsheet_key",
         "type": str,
         "help": """A Google Spreadsheet name, containing keys to be deleted.
-                   The column must contain a (any) header!"""
+                   The columns must contain (any) headers!"""
     }]
     ui = Interface(__file__,
                    """This script will delete a file and all of its documents,
@@ -52,34 +55,67 @@ def main():
             ui.args.google_spreadsheet_key,
             settings.google_client_email,
             settings.google_p12_file)
-    elif ui.args.key is not None:
-        ui.info("Using key `%s`" % ui.args.key)
-        data_set = DataSet([{"key": ui.args.key}])
+    elif ui.args.path_fragments is not None:
+        ui.info("Using key `%s`" % ui.args.path_fragments)
+        data_set = HeaderlessDataSet([ui.args.path_fragments])
     else:
-        ui.error("""You must specify either a file,
-                    a Google Spreadsheet, or a key""")
+        ui.error("""You must specify either a file, a Google Spreadsheet,\
+                    or a key. Run `./erase --help` for more info""")
         ui.exit()
-
-    for row in data_set.get_next():
-        print row.itervalues().next()
-    ui.exit()
 
     ui.info("Setting up db connection.")
     try:
-        db = settings.Database(settings.db_server,
-                               settings.db_harvest_table,
-                               "info",
-                               port=settings.db_port
-                               )
+        file_db = settings.Database(settings.db_server,
+                                    settings.db_harvest_table,
+                                    "info",
+                                    port=settings.db_port)
+        docs_db = settings.Database(settings.db_server,
+                                    settings.db_extactor_table,
+                                    "info",
+                                    port=settings.db_port)
     except (TypeError, NameError, AttributeError):
         ui.info("No database setup found, using DebuggerDB")
-        db = DebuggerDB(None, settings.db_harvest_table or "TABLE")
+        file_db = DebuggerDB(None, settings.db_harvest_table or "TABLE")
+        docs_db = DebuggerDB(None, settings.db_extactor_table or "TABLE")
 
     ui.info("Connecting to file storage")
-    storage = settings.Storage(settings.access_key_id,
-                               settings.secret_access_key,
-                               settings.access_token,
-                               settings.bucket_name)
+    file_storage = settings.Storage(settings.access_key_id,
+                                    settings.secret_access_key,
+                                    settings.access_token,
+                                    settings.bucket_name)
+
+    ui.info("Connecting to document storage")
+    docs_storage = settings.Storage(settings.access_key_id,
+                                    settings.secret_access_key,
+                                    settings.access_token,
+                                    settings.text_bucket_name)
+
+    for row in data_set.get_next():
+        path_fragments = row.list()  # ["Ale kommun", "abc.pdf"]
+        db_key = file_db.create_key(path_fragments)  # "Ale kommun-abc.pdf"
+        filename = path_fragments.pop()  # "abc.pdf"
+        # path_fragments: ["Ale kommun"]
+        file_path = file_storage.buildRemoteName(filename, path=path_fragments)
+        file_path_from_db = file_db.get(db_key, "storage_path")
+
+        # Double check that reconstructed file match matches that in db.
+        # File paths was not always stored in db, so might very well be missing
+        if file_path != file_path_from_db:
+            ui.warning("File paths from storage and db do not match")
+            ui.debug("Storage: %s; Db: %s" % (file_path, file_path_from_db))
+            if not ui.ask_if_continue():
+                ui.exit()
+        docs = docs_db.get_attribute_with_value("file", db_key)
+        if len(docs) == 0:
+            message = "No documents for %s. Continue deleting only file?" % \
+                      filename
+            if not ui.ask_if_continue(message=message):
+                ui.exit()
+        file_db.delete(db_key)
+        file_storage.delete_file(file_path)
+        #for doc in docs:
+        #   doc
+        #   docs_storage.delete(create_key(get(doc, file)))
 
 
 if __name__ == '__main__':
