6a8a26da37810d34b73030b244cd3b83d2ba4bcb
rotsee <leo_wallentin@hotmail.com>
2015-02-24 09:37:28 +0100

    clean up PDF extractor

--- modules/extractors/pdf.py
+++ modules/extractors/pdf.py
@@ -9,31 +9,22 @@ from modules.extractors.documentBase import ExtractorBase, Page
 from modules.metadata import Metadata
 from modules.xmp import xmp_to_dict
 from modules.utils import make_unicode
+from modules.extractors.pdfUtils import Stream
 
-from binascii import b2a_hex
-import zlib
 from PIL import Image
-from os import sep as os_sep
+from os import unlink
 from tempfile import NamedTemporaryFile
+
+from pytesseract import image_to_string
 
 from pdfminer.pdfparser import PDFParser
 from pdfminer.pdfdocument import PDFDocument
-from pdfminer.pdfpage import PDFPage
-from pdfminer.pdfpage import PDFTextExtractionNotAllowed
-from pdfminer.pdfinterp import PDFResourceManager
-from pdfminer.pdfinterp import PDFPageInterpreter
-from pdfminer.ccitt import ccittfaxdecode
-
-from pdfminer.layout import LAParams, LTTextBox, LTTextLine, LTFigure, LTImage, LTRect
+from pdfminer.pdfpage import PDFPage, PDFTextExtractionNotAllowed
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.layout import LAParams, LTTextBox, LTTextLine, LTFigure, LTImage
 from pdfminer.converter import PDFPageAggregator
-from pdfminer.pdftypes import resolve1, PDFObjRef
-
-hexadecimal = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6,
-               '7': 7, '8': 8, '9': 9, 'a': 10, 'b': 11, 'c': 12,
-               'd': 13, 'e': 14, 'f': 15}
-base85m4 = long(pow(85, 4))
-base85m3 = long(pow(85, 3))
-base85m2 = long(pow(85, 2))
+from pdfminer.pdftypes import resolve1
+from pdfminer.image import ImageWriter
 
 
 class PDFError(Exception):
@@ -41,241 +32,41 @@ class PDFError(Exception):
 
 
 class PdfImage(object):
-    """Class representing an image in a PDF.
-       Contains methods for extracting images.
-    """
+    """Class representing an image in a PDF."""
 
     def __init__(self, image_obj):
-        """Represents a PDFMiner LTImage object.
-           Extracts the actual image data.
-        """
+        """Takes a PDFMiner LTImage object."""
         if image_obj.stream is None:
             raise Exception("No image stream")
-        if self._clean_up_stream_attribute(image_obj.stream["Subtype"]) != "image":
+
+        self.stream = Stream(image_obj.stream)
+        if self.stream.get("Subtype") != "image":
             raise Exception("Not an image")
 
-        self.width = image_obj.stream["Width"]
-        self.height = image_obj.stream["Height"]
-        if "F" in image_obj.stream:
-            self._filter = self._clean_up_stream_attribute(image_obj.stream["F"])
-        else:
-            self._filter = self._clean_up_stream_attribute(image_obj.stream["Filter"])
-        if "DP" in image_obj.stream:
-            self._filter_params = image_obj.stream["DP"]
-        elif "DecodeParms" in image_obj.stream:
-            self._filter_params = image_obj.stream["DecodeParms"]
-        elif "FDecodeParms" in image_obj.stream:
-            self._filter_params = image_obj.stream["FDecodeParms"]
-
-
-        self._bits = image_obj.stream["BitsPerComponent"]
-        self._raw_data = image_obj.stream.get_rawdata()
-        if self._filter is not None:
-            self._decompress()
-        if "CS" in image_obj.stream:
-            self.colorspace = image_obj.stream["CS"]
-        elif "ColorSpace" in image_obj.stream:
-            self.colorspace = image_obj.stream["ColorSpace"]
-        else:
-            self.colorspace = "DeviceGray"
-
-        if isinstance(self.colorspace, PDFObjRef):
-            self.colorspace = self.colorspace.resolve()
-        self.color_mode = self.get_colormode(self.colorspace,
-                                             bits=self._bits)
-        if self.color_mode is None:
-            print self.colorspace
-            raise Exception("No method for handling colorspace")
+        self._stream = image_obj.stream
+        self._image_obj = image_obj
 
     def get_text(self):
-        """Does OCR on this image.
-        """
-        # Some images are flipped. I have no idea how to detect this
-        # with PDF Minder, so for now, also OCR the flipped image
-        temp_image = self._get_image()
-        if temp_image is not None:
-            import pytesseract
-            text = pytesseract.image_to_string(temp_image, lang="swe")
-            text += " \n"
-            rotated_image = temp_image.transpose(Image.FLIP_TOP_BOTTOM)
-            text += pytesseract.image_to_string(rotated_image, lang="swe")
+        """Does OCR on this image."""
+        image_writer = ImageWriter("temp")
+        temp_image = image_writer.export_image(self._image_obj)
+        try:
+            text = image_to_string(Image.open("temp/" + temp_image),
+                                   lang="swe")
+            unlink("temp/" + temp_image)
             return text
-
-    def _get_image(self):
-        """Return an image from this image data.
-        """
-        temp_image = None
-        try:
-            # Assume war image data
-            temp_image = Image.frombuffer(self.color_mode,
-                                          (self.width, self.height),
-                                          self._raw_data, "raw",
-                                          self.color_mode, 0, 1)
-        except Exception:
-            # Not raw image data.
-            # Can we make sense of this stream some other way?
-            try:
-                import StringIO
-                temp_image = Image.open(StringIO.StringIO(self._raw_data))
-            except Exception:
-                # PIL failed us. Try to print data to a file, and open it
-                file_ext = self._determine_image_type(self._raw_data[0:4])
-                if file_ext:
-                    # TODO use tempfile
-                    file_name = os_sep.join(["header", file_ext])
-                    with open("temp/" + file_name, "w") as image_file:
-                        image_file.write(self._raw_data)
-                    temp_image = Image.open(image_file)
-
-        return temp_image or None
-
-    def get_colormode(self, color_space, bits=None):
-        color_mode = None
-        if isinstance(color_space, list):
-            color_space_family = self._clean_up_stream_attribute(color_space[0])
-        else:
-            color_space_family = self._clean_up_stream_attribute(color_space)
-
-        if color_space_family == "indexed":
-            color_schema = color_space[1]
-            if isinstance(color_schema, PDFObjRef):
-                color_schema = color_schema.resolve()
-            if isinstance(color_schema, list):
-                color_schema = color_schema[0]
-            color_schema = self._clean_up_stream_attribute(color_schema)
-
-            bits = color_space[2] or bits
-            if isinstance(bits, PDFObjRef):
-                bits = bits.resolve()
-
-            if color_schema == "devicegray" and bits == 1:
-                color_mode = "1"
-            elif color_schema == "devicegray" and bits == 8:
-                color_mode = "L"
-            elif color_schema == "iccbased":
-                # FIXME This just happens to work often enough. We should
-                # let PDFMiner take care of all this work, though, rather
-                # than implementníng all the logic (this is complex!) ourselves
-                color_mode = "L"
-        elif color_space_family == "pattern":
-            pass
-        elif color_space_family == "separation":
-            pass
-        elif color_space_family == "devicen":
-            pass
-        elif color_space_family == "calgray":
-            pass
-        elif color_space_family == "calrgb":
-            pass
-        elif color_space_family == "lab":
-            pass
-        elif color_space_family == "iccbased":
-            color_mode = "L"
-        elif color_space_family == "devicegray":
-            if bits == 8:
-                color_mode = "L"
-            else:
-                color_mode = "1"
-        elif color_space_family == "devicergb":
-            color_mode = "RGB"
-        elif color_space_family == "devicecmyk":
-            pass
-        return color_mode
-
-    def _decompress(self):
-        """Decompress the image raw data in this image
-        """
-        if self._filter == 'asciihexdecode':
-            self._raw_data = self._asciihexdecode(self._raw_data)
-        elif self._filter == 'ascii85decode':
-            self._raw_data = self._ascii85decode(self._raw_data)
-        elif self._filter == 'flatedecode':
-            self._raw_data = zlib.decompress(self._raw_data)
-        elif self._filter == "ccittfaxdecode":
-            self._raw_data = ccittfaxdecode(self._raw_data, self._filter_params)
-        return None
-
-    def _clean_up_stream_attribute(self, attribute):
-        try:
-            return str(attribute).strip("/_").lower()
-        except Exception:
-            return None
-
-    def _determine_image_type(self, stream_first_4_bytes):
-        """Find out the image file type based on the magic number"""
-        file_type = None
-        bytes_as_hex = b2a_hex(stream_first_4_bytes)
-        if bytes_as_hex.startswith('ffd8'):
-            file_type = 'jpeg'
-        elif bytes_as_hex == '89504e47':
-            file_type = 'png'
-        elif bytes_as_hex == '47494638':
-            file_type = 'gif'
-        elif bytes_as_hex.startswith('424d'):
-            file_type = 'bmp'
-        return file_type
-
-    def _clean_hexadecimal(self, a):
-        """ Read the string, converting the pairs of digits to
-            characters
-        """
-        b = ''
-        shift = 4
-        value = 0
-
-        try:
-            for i in a:
-                value = value | (hexadecimal[i] << shift)
-                shift = 4 - shift
-                if shift == 4:
-                    b = b + chr(value)
-                    value = 0
-        except ValueError:
-            raise PDFError("Problem with hexadecimal string %s" % a)
-        return b
-
-    def _asciihexdecode(self, text):
-        at = text.find('>')
-        return self._clean_hexadecimal(text[:at].lower())
-
-    def _ascii85decode(self, text):
-        end = text.find('~>')
-        new = []
-        i = 0
-        ch = 0
-        value = 0
-        while i < end:
-            if text[i] == 'z':
-                if ch != 0:
-                    raise PDFError('Badly encoded ASCII85 format.')
-                new.append('\000\000\000\000')
-                ch = 0
-                value = 0
-            else:
-                v = ord(text[i])
-                if v >= 33 and v <= 117:
-                    if ch == 0:
-                        value = ((v - 33) * base85m4)
-                    elif ch == 1:
-                        value = value + ((v - 33) * base85m3)
-                    elif ch == 2:
-                        value = value + ((v - 33) * base85m2)
-                    elif ch == 3:
-                        value = value + ((v - 33) * 85)
-                    elif ch == 4:
-                        value = value + (v - 33)
-                        c1 = int(value >> 24)
-                        c2 = int((value >> 16) & 255)
-                        c3 = int((value >> 8) & 255)
-                        c4 = int(value & 255)
-                        new.append(chr(c1) + chr(c2) + chr(c3) + chr(c4))
-                    ch = (ch + 1) % 5
-            i = i + 1
-        if ch != 0:
-            c = chr(value >> 24) + chr((value >> 16) & 255) + \
-                chr((value >> 8) & 255) + chr(value & 255)
-            new.append(c[:ch - 1])
-        return "".join(new)
+        except IOError:
+            # PdfMiner did not return an image
+            # Let's try to create one ourselves
+            # TODO: Create proper color_mode values from ColorSpace
+            # Most of the times "L" will create something good enough
+            # for OCR, though
+            temp_image = Image.frombuffer("L",
+                                          self._image_obj.srcsize,
+                                          self._stream.get_data(), "raw",
+                                          "L", 0, 1)
+            text = image_to_string(temp_image, lang="swe")
+            return text
 
 
 class PdfPage(Page):
@@ -365,8 +156,6 @@ class PdfPage(Page):
             # An image
             image = PdfImage(lt_obj)
             text_content.append(image.get_text())
-        elif isinstance(lt_obj, LTRect):
-            pass
         try:
             return ''.join(text_content)
         except TypeError:
@@ -402,7 +191,6 @@ class PdfPageFromOcr(PdfPage):
 
     def do_ocr(self):
         import subprocess
-        import os
         temp_filename = os.path.join("temp", "ocr.png")
         try:
             arglist = ["gs",
@@ -418,15 +206,13 @@ class PdfPageFromOcr(PdfPage):
         except OSError as e:
             logging.error("Failed to run GhostScript.\
                            I/O error({0}): {1}".format(e.errno, e.strerror))
-        #Do OCR
+        # Do OCR
         import time
         time.sleep(1)  # make sure the server has time to write the files
-        #import Image
-        import pytesseract
-        text = pytesseract.image_to_string(
+        text = image_to_string(
             Image.open(temp_filename),
             lang="swe")
-        os.unlink(temp_filename)
+        unlink(temp_filename)
         return text
 
 
