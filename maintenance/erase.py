e357a6dcaca68460b830df2c9441163bcc5b246a
rotsee <leo_wallentin@hotmail.com>
2015-03-13 23:44:33 +0100

    erase both files and documents

--- maintenance/erase.py
+++ maintenance/erase.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python2
-#coding=utf-8
+# -*- coding: utf-8 -*-
 """This script will delete a file and all of its documents,
    from both the storage, and the database.
 
@@ -43,7 +43,7 @@ def main():
     ui = Interface(__file__,
                    """This script will delete a file and all of its documents,
                       from both the storage, and the database.""",
-                   commandLineArgs=command_line_args)
+                   commandline_args=command_line_args)
 
     if ui.args.filename is not None:
         ui.info("Reading keys from `%s`" % ui.args.filename)
@@ -63,7 +63,7 @@ def main():
                     or a key. Run `./erase --help` for more info""")
         ui.exit()
 
-    ui.info("Setting up db connection.")
+    ui.info("Setting up db connections")
     try:
         file_db = settings.Database(settings.db_server,
                                     settings.db_harvest_table,
@@ -78,13 +78,11 @@ def main():
         file_db = DebuggerDB(None, settings.db_harvest_table or "TABLE")
         docs_db = DebuggerDB(None, settings.db_extactor_table or "TABLE")
 
-    ui.info("Connecting to file storage")
+    ui.info("Connecting to storages")
     file_storage = settings.Storage(settings.access_key_id,
                                     settings.secret_access_key,
                                     settings.access_token,
                                     settings.bucket_name)
-
-    ui.info("Connecting to document storage")
     docs_storage = settings.Storage(settings.access_key_id,
                                     settings.secret_access_key,
                                     settings.access_token,
@@ -106,20 +104,35 @@ def main():
             if not ui.ask_if_continue():
                 ui.exit()
 
-        docs = docs_db.get_attribute_with_value("file", db_key)
-        if len(docs) == 0:
-            message = "No documents for %s. Continue deleting only file?" % \
-                      filename
-            if not ui.ask_if_continue(message=message):
+        docs_db_ids = docs_db.get_attribute_with_value("file",
+                                                       file_path_from_db)
+
+        num_docs_in_storage = docs_storage.get_file_list_length(file_path)
+        if num_docs_in_storage != len(docs_db_ids):
+            ui.warning("Doc count in storage and db do not match")
+            if not ui.ask_if_continue():
                 ui.exit()
 
+        if len(docs_db_ids) == 0:
+            ui.info("No documents for %s. Deleting only files" % file_path)
+
+        ui.info("Deleting file db entry %s" % db_key)
         if ui.executionMode < Interface.DRY_MODE:
             file_db.delete(db_key)
-            file_storage.delete_file(file_path)
-            #for doc in docs:
-            #   doc
-            #   docs_storage.delete(create_key(get(doc, file)))
 
+        ui.info("Deleting file %s" % file_path)
+        if ui.executionMode < Interface.DRY_MODE:
+            file_storage.delete_file(file_path)
+
+        for docs_db_id in docs_db_ids:
+            doc_file = docs_db.get(docs_db_id, "text_file")
+            ui.info("Deleting document file %s" % doc_file)
+            if ui.executionMode < Interface.DRY_MODE:
+                docs_storage.delete_file(doc_file)
+
+            ui.info("Deleting document db entry %s" % docs_db_id)
+            if ui.executionMode < Interface.DRY_MODE:
+                docs_db.delete(docs_db_id)
 
 if __name__ == '__main__':
     main()
